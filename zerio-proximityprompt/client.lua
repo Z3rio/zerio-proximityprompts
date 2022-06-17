19225ba008899132df6623f4867bcadf19d90a0b
Zerio <54480523+Z3rio@users.noreply.github.com>
2022-06-17 11:32:16 +0200

    Add files via upload

---
+++ zerio-proximityprompt/client.lua
@@ -0,0 +1,246 @@
+Citizen.CreateThread(function()
+    local PlayerData = nil
+    local loaded = false
+    local prompts = {}
+    local usagefuncs = {}
+    local defaultdata = {
+        name = "testprompt",
+        objecttext = "World",
+        actiontext = "Interact with something",
+        holdtime = 5000,
+        key = "E",
+        position = vector3(0.0, 0.0, 0.0),
+        params = {"test", "data"},
+        usage = function(data)
+            print("Proximity prompt got used, the following params got passed: " .. json.encode(data))
+        end,
+        drawdist = 3,
+        usagedist = 1.5
+    }
+
+    RegisterNUICallback("triggercallback", function(data, cb)
+        usagefuncs[data.name](prompts[data.name].params)
+        cb()
+    end)
+
+    RegisterNUICallback("loaded", function(data, cb)
+        loaded = true
+        SendNUIMessage({
+            action = "loaded"
+        })
+        cb()
+    end)
+
+    function AddNewPrompt(data)
+        if data == nil then
+            data = defaultdata
+        end
+
+        for i, v in pairs(defaultdata) do
+            if data[i] == nil then
+                data[i] = v
+            end
+        end
+
+        data.timeheld = 0
+        data.visible = false
+        data.key = string.upper(data.key)
+        data.isbeingpressed = false
+        data.holding = false
+        data.left = 0
+        data.top = 0
+
+        if Keys[data.key] ~= nil then
+            if prompts[data.name] == nil then
+                prompts[data.name] = data
+                usagefuncs[data.name] = data.usage
+
+                local data2 = data
+                data2.usage = nil
+
+                if loaded then
+                    SendNUIMessage({
+                        action = "addnewprompt",
+                        data = data
+                    })
+                else
+                    CreateThread(function()
+                        while loaded == false do
+                            Citizen.Wait(1000)
+                        end
+                        SendNUIMessage({
+                            action = "addnewprompt",
+                            data = data
+                        })
+                        return
+                    end)
+                end
+
+                local subfuncs = {}
+                function subfuncs:Remove()
+                    prompts[data.name] = nil
+                end
+                return subfuncs
+            else
+                print(string.format(
+                    "ZERIO-PROXIMITYPROMPT [WARN] - There is already a proximity prompt with the id \"%s\"", data.name))
+            end
+        else
+            print(string.format(
+                "ZERIO-PROXIMITYPROMPT [WARN] - The key \"%s\" doesn't exist. This warning is from the following prompt: \"%s\"",
+                data.key, data.name))
+        end
+    end
+
+    exports("AddNewPrompt", AddNewPrompt)
+
+    while loaded == false do
+        Citizen.Wait(100)
+    end
+
+    if GetResourceState("qb-core") ~= "missing" then
+        QBCore = exports["qb-core"]:GetCoreObject()
+
+        PlayerData = QBCore.Functions.GetPlayerData()
+        if PlayerData and PlayerData.job then
+            SendNUIMessage({
+                action = "updatejob",
+                job = PlayerData.job.name
+            })
+        end
+
+        RegisterNetEvent("QBCore:Client:OnJobUpdate")
+        AddEventHandler("QBCore:Client:OnJobUpdate", function()
+            PlayerData = QBCore.Functions.GetPlayerData()
+            SendNUIMessage({
+                action = "updatejob",
+                job = PlayerData.job.name
+            })
+        end)
+
+        RegisterNetEvent("QBCore:Client:OnPlayerLoaded")
+        AddEventHandler("QBCore:Client:OnPlayerLoaded", function()
+            PlayerData = QBCore.Functions.GetPlayerData()
+            SendNUIMessage({
+                action = "updatejob",
+                job = PlayerData.job.name
+            })
+        end)
+    end
+
+    if GetResourceState("es_extended") ~= "missing" then
+        ESX = nil
+        while ESX == nil do
+            TriggerEvent("esx:getSharedObject", function(obj)
+                ESX = obj
+            end)
+            Citizen.Wait(0)
+        end
+
+        PlayerData = ESX.GetPlayerData()
+        if PlayerData and PlayerData.job then
+            SendNUIMessage({
+                action = "updatejob",
+                job = PlayerData.job.name
+            })
+        end
+
+        RegisterNetEvent("esx:setJob")
+        AddEventHandler("esx:setJob", function()
+            PlayerData = ESX.GetPlayerData()
+            SendNUIMessage({
+                action = "updatejob",
+                job = PlayerData.job.name
+            })
+        end)
+
+        RegisterNetEvent("esx:playerLoaded")
+        AddEventHandler("esx:playerLoaded", function()
+            PlayerData = ESX.GetPlayerData()
+            SendNUIMessage({
+                action = "updatejob",
+                job = PlayerData.job.name
+            })
+        end)
+    end
+
+    while true do
+        local plrpos = GetEntityCoords(PlayerPedId())
+        local resx, resy = GetActiveScreenResolution()
+        local idx = -1
+
+        local noneInRange, noneOnScreen = true, true
+
+        for i, v in pairs(prompts) do
+            local v = prompts[i]
+            local dist = #(plrpos - v.position)
+            if dist < v.drawdist then
+                noneInRange = false
+                local onscreen, x, y = World3dToScreen2d(v.position.x, v.position.y, v.position.z)
+                if onscreen then
+                    noneOnScreen = false
+                    prompts[i].left = (x * resx) * 0.75
+                    prompts[i].top = y * resy
+                    prompts[i].visible = true
+                    prompts[i].scale = (1 / dist)
+
+                    if prompts[i].scale > 1 then
+                        prompts[i].scale = 1
+                    end
+                    if prompts[i].scale < 0.5 then
+                        prompts[i].scale = 0.5
+                    end
+
+                    if IsControlPressed(0, Keys[v.key]) and prompts[i].isbeingpressed == false and v.usagedist > dist then
+                        prompts[i].isbeingpressed = true
+                        Citizen.CreateThread(function()
+                            local key = Keys[v.key]
+
+                            SendNUIMessage({
+                                action = "startholding",
+                                idx = i
+                            })
+
+                            while true do
+                                if not IsControlPressed(0, key) then
+                                    prompts[i].isbeingpressed = false
+                                    SendNUIMessage({
+                                        action = "stopholding",
+                                        idx = i
+                                    })
+                                    return
+                                end
+                                Citizen.Wait(100)
+                            end
+                        end)
+                    end
+                else
+                    prompts[i].visible = false
+                end
+            else
+                prompts[i].visible = false
+            end
+
+            SendNUIMessage({
+                action = "updateprompt",
+                idx = i,
+                data = {
+                    visible = prompts[i].visible,
+                    top = prompts[i].top,
+                    left = prompts[i].left,
+                    scale = prompts[i].scale
+                }
+            })
+        end
+
+        if noneOnScreen == false then
+            Citizen.Wait(Config.RefreshRate)
+        else
+            Citizen.Wait(100)
+        end
+
+        if noneInRange == true then
+            Citizen.Wait(250)
+        end
+    end
+end)
